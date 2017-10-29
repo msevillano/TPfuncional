42e1f4b3b87a0e29a8e68fcd7db9d47cc2f62038
Msevillano <sevillano.mauro@gmail.com>
2017-10-29 14:44:36 -0300

    Correccion del TP

--- TPfuncional.hs
+++ TPfuncional.hs
@@ -1,115 +1,127 @@
 import Text.Show.Functions
-import Data.List
 
--- Modelo de datos (Gimnasta y rutina)
-type Ejercicio = (Gimnasta -> Gimnasta)
+-- Punto 1 
+type Ejercicio = Gimnasta -> Gimnasta
+type RutinaEjercicio = (Int,Ejercicio)
+type Rutina = (Int,[RutinaEjercicio])
 
-data Gimnasta = Gimnasta {
+data Gimnasta = Gimnasta 
+{
     nombre :: String,
     energia :: Int,
     equilibrio :: Int,
     flexibilidad :: Int,
     fuerza :: Int,
-    habilidad :: [Gimnasta -> Gimnasta]
-}
+    ejercicios :: [Ejercicio]
+} deriving (Show)
 
-data Rutina = Rutina{
-    repeticiones :: Int,
-    habilidades :: [Gimnasta -> Gimnasta]
-}
--- ejercicios
+aumentarEnergia :: Int -> Gimnasta -> Gimnasta
+aumentarEnergia cantidad gimnasta = gimnasta {energia = energia gimnasta + cantidad}
 
-medialuna :: Ejercicio
-medialuna gimnasta = Gimnasta {
-    equilibrio = equilibrio gimnasta + 5
-}
+aumentarEquilibrio :: Int -> Gimnasta -> Gimnasta
+aumentarEquilibrio cantidad gimnasta = gimnasta {equilibrio = equilibrio gimnasta + cantidad}
 
-rolAdelante :: Int -> Ejercicio
-rolAdelante gimnasta velocidad = Gimnasta {
-    energia = energia gimnasta + (round $ fromIntegral (velocidad / 2))
-}
+umentarFlexibilidad :: Int -> Gimnasta -> Gimnasta
+aumentarFlexibilidad cantidad gimnasta = gimnasta {flexibilidad = flexibilidad gimnasta + cantidad}
 
-vertical :: Ejercicio
-vertical gimnasta = Gimnasta {
-    fuerza = fuerza gimnasta + 5
-}
+aumentarFuerza :: Int -> Gimnasta -> Gimnasta
+aumentarFuerza cantidad gimnasta = gimnasta {fuerza = fuerza gimnasta + cantidad}
 
-saltoConSoga :: Int -> Ejercicio
-saltoConSoga gimnasta saltos = Gimnasta {
-    fuerza = fuerza gimnasta + (round $ fromIntegral saltos),
-    energia = energia gimnasta - (round $ fromIntegral (saltos / 2))
-}
+medialuna gimnasta = aumentarEquilibrio 5 gimnasta
+rolAdelante velocidad gimnasta = aumentarEnergia (div velocidad 2) gimnasta
+vertical gimnasta = aumentarFuerza 7 gimnasta
+saltoConSoga cantSaltos gimnasta = (aumentarFuerza cantSaltos.aumentarEnergia (-div cantSaltos 2)) gimnasta
+saltoMortal altura impulso gimnasta = (aumentarFuerza altura.aumentarFlexibilidad (div impulso 2)) gimnasta
 
-saltoMortal :: Int -> Int -> Ejercicio
-saltoMortal altura impulso gimnasta = Gimnasta {
-    flexibilidad = flexibilidad gimnasta * altura,
-    fuerza = fuerza gimnasta + altura
-}
--- Sonia y Pedro
+-- Punto 2 
+sonia = Gimnasta "sonia" 90 60 40 50 [medialuna,rolAdelante 20,saltoMortal 40 15]
+pedro = Gimnasta "pedro" 70 50 50 60 [saltoConSoga 150,vertical,rolAdelante 30]
 
-sonia = Gimnasta {
-    nombre = "Sonia",
-    energia = 90,
-    equilibrio = 60,
-    flexibilidad = 40,
-    fuerza = 50,
-    habilidad = [medialuna, rolAdelante 20, saltoMortal 50 15]
-}
+-- Punto 3
+-- Ej A)
+aprender :: Ejercicio -> Gimnasta -> Gimnasta
+aprender ejercicio gimnasta = gimnasta {ejercicios = (ejercicio:(ejercicios gimnasta))}
 
-pedro = Gimnasta {
-    nombre = "Pedro",
-    energia = 70,
-    equilibrio = 50,
-    flexibilidad = 50,
-    fuerza = 60,
-    habilidad = [saltoConSoga 150, vertical, rolAdelante 30]
-}
--- ejercitar
+ejercitar :: Int -> Ejercicio -> Gimnasta -> Gimnasta
+ejercitar cantMinutos ejercicio gimnasta | (div cantMinutos 2) > 0 = ejercitar (cantMinutos -2) ejercicio (ejercicio gimnasta)
+                                         | otherwise = aprender ejercicio gimnasta
 
-ejercitar gimnasta ejercicio tiempo {
-    |tiempo < 120 = habilidad gimnasta = [ejercicio:habilidad gimnasta]
-    |otherwise = ejercicio gimnasta , tiempo = tiempo - 120, ejercitar gimnasta ejercicio (tiempo - 120)
-}
--- rutinas
+-- Ej B)
+entradaCalor :: Rutina
+entradaCalor = (2, [(2 ,rolAdelante 10), (4, medialuna), (1,saltoConSoga 50), (1,saltoMortal 20 15)])
 
-entradaEnCalor = Rutina {
-    repeticiones = 2,
-    habilidades = [rolAdelante 10, rolAdelante 10, medialuna, medialuna, medialuna, medialuna, saltoConSoga 50, saltoMortal 20 15]
-}
+rutinaDiaria :: Rutina
+rutinaDiaria = (3, [(1,rolAdelante 20),(1,saltoConSoga 30),(1,vertical),(1,medialuna),(1,saltoConSoga 10)])
 
-rutinaDiaria = Rutina {
-    repeticiones = 3,
-	habilidades = [rolAdelante 20, saltoConSoga 30, vertical, medialuna, saltoConSoga 10]
-}
--- entrenar
+-- Ej C)
+repetir :: Int -> (Gimnasta -> Gimnasta) -> Gimnasta -> Gimnasta
+repetir 0 _ gimnasta = gimnasta
+repetir repeticiones funcion gimnasta = repetir (repeticiones-1) funcion (funcion gimnasta)
 
-entrenar gimnasta rutina{
-    |repeticiones = 0
-    |otherwise hacerUnEjercicio gimnasta habilidades, repeticiones = repeticiones - 1
-}
-hacerUnEjercicio gimnasta [ejercicio] {
-    | head ejercicio = null
-    | otherwise = head(ejercicio) gimnasta, take 1 ejercicio
-}
--- potencial
+realizarEjercicioRut :: Gimnasta -> RutinaEjercicio -> Gimnasta
+realizarEjercicioRut gimnasta (repeticiones,ejercicio) = repetir repeticiones ejercicio gimnasta
 
--- maximos y minimos
+realizarEjerciciosRut :: [RutinaEjercicio] -> Gimnasta -> Gimnasta
+realizarEjerciciosRut ejercicios gimnasta = foldl realizarEjercicioRut gimnasta ejercicios
 
-maxFuerza [gimnasta] = gimnasta 
-maxFuerza [x]    = x
-maxFuerza (x:xs) = fuerza x max fuerza xs
+entrenar :: Rutina -> Gimnasta -> Gimnasta
+entrenar (repeticiones,ejercicios) gimnasta = repetir repeticiones (realizarEjerciciosRut ejercicios) gimnasta
 
+-- Ej D)
+nivelFortaleza :: Gimnasta -> Int
+nivelFortaleza (Gimnasta nombre energia equilibrio flexibilidad fuerza ejercicios) = energia+fuerza
 
-minFuerza [gimnasta] = gimnasta 
-minFuerza [x] = x
-minFuerza (x:xs) = fuerza x min fuerza xs
+realizarEjerciciosPers :: Gimnasta -> Gimnasta
+realizarEjerciciosPers gimnasta = foldr ($) gimnasta (ejercicios gimnasta) 
 
-minFlex [gimnasta] = gimnasta 
-minFlex [x] = x
-minFlex (x:xs) = flex x min flex xs
+tienePotencial :: Int -> Gimnasta -> Bool
+tienePotencial n gimnasta = (nivelFortaleza.realizarEjerciciosPers.entrenar rutinaDiaria) gimnasta > n
 
--- una función h es una subrutina definida que no está enlazada a un identificador.
--- inferencia de tipos y lazy evaluation.
+-- Punto 4
+-- A)
+nombreMaxGenericoRutina :: (Gimnasta -> Int) -> Rutina -> [Gimnasta] -> String
+nombreMaxGenericoRutina criterio_max rutina gimnastas = (nombre.maxSegun criterio_max.entrenarGimnastasRutina rutina) gimnastas
 
--- 6a No, de agregarlo inteferiria en el punto 4c y el que ya sabe el ejercicio no lo haria.
--- 6b Si, si la condicion de fin es encontrar a un socio en particular.
+entrenarGimnastasRutina :: Rutina -> [Gimnasta] -> [Gimnasta]
+entrenarGimnastasRutina rutina gimnastas = map (entrenar rutina) gimnastas
+
+maxSegun :: Ord b => (Gimnasta -> b) -> [Gimnasta] -> Gimnasta
+maxSegun _ [elemento] = elemento
+maxSegun funcion (x:y:xs) | funcion x > funcion y = maximoSegun funcion(x:xs)
+                          | otherwise = maximoSegun funcion(y:xs)
+
+maxDespuesDeRutina :: [Gimnasta] -> String
+maxDespuesDeRutina gimnastas = nombreMaxGenericoRutina fuerza rutinaDiaria gimnastas
+
+-- B)
+fortaleza :: Gimnasta -> Int
+fortaleza gimnasta = (energia gimnasta) + (fuerza gimnasta)
+
+minEntreDos :: (Gimnasta->Int) -> (Gimnasta->Int) -> Gimnasta -> Int
+minEntreDos funcion1 funcion2 gimnasta | (funcion1 gimnasta) > (funcion2 gimnasta) = funcion2 gimnasta
+                                       | otherwise = funcion1 gimnasta
+
+maxConMin :: [Gimnasta] -> String
+maxConMin gimnastas = nombreMaxGenericoRutina (minimoEntreDos flexibilidad fortaleza) rutinaDiaria gimnastas
+
+-- C)
+entrenarEjercicio :: Ejercicio -> Int -> [Gimnasta] -> [Gimnasta]
+entrenarEjercicio ejercicio cantMinutos gimnastas = map (ejercitar cantMinutos ejercicio) gimnastas
+
+cantidadEjercicios :: Gimnasta -> Int
+cantidadEjercicios gimnasta = (length.ejercicios) gimnasta
+
+nombreMaxGenericoEjercicio :: (Gimnasta -> Int) -> Ejercicio -> Int -> [Gimnasta] -> String
+nombreMaxGenericoEjercicio criterio_max ejercicio cantMinutos gimnastas = (nombre.maxSegun criterio_max.entrenarConEjercicio ejercicio cantMinutos) gimnastas
+
+maxDespuesDeEjercicio :: [Gimnasta] -> Int -> Ejercicio -> String
+maxDespuesDeEjercicio gimnastas cantMinutos ejercicio = nombreMaxGenericoEjercicio (cantidadEjercicios) ejercicio cantMinutos gimnastas
+
+-- Punto 5
+--           Se utilizan Expresiones Lambda y composicion de funciones.
+--           El parametro e es comparable, ya que esta definido como Eq, el parametro g es una Expresion Lambda y una lista de los elementos iguales a e
+
+-- Punto 6
+--           A_ No se puede implementar ya que el type Ejercicio no es de Eq
+--           B_ Si la funcion termina si y solo si se cumple con la condicion, por ejemplo buscar algun socio que tenga 100 de fuerza en la lista de gimnastas
+--              h 100 (\n->n fuerza) listaInfinita
